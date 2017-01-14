959a546457e5681f61d0cf31c3bc1a4cf6b2e075
Tom Lous <tomlous@gmail.com>
2017-01-14 23:27:15 +0100

    Tried ROT47, Base64 and substitution

--- decrypt.py
+++ decrypt.py
@@ -1,5 +1,6 @@
 import sys
 import binascii
+import operator
 
 
 def main():
@@ -7,8 +8,38 @@ def main():
 
     decodedString = ""
 
-    cypherText = "HXvfWs@4$#&Rty Pkm!nm348LgM**% GfrZ33?/?Rtc96"
-    cypherText = "HXvfW5@4$#&Rty Pkm!nm348LgM**% GfrZ33?/?Rtc96"
+    #cypherText = "HXvfWs@4$#&Rty Pkm!nm348LgM**% GfrZ33?/?Rtc96"
+    cypherText = "HXvfW5@4$#&RtyPkm!nm348LgM**%GfrZ33?/?Rtc96"
+
+    # 44 chars
+
+    # Tried:
+    # Not base64 (invalid chars)
+    # Not ROT47
+
+    asciiNums = []
+    charCount = {}
+
+    print "char\tasciidec"
+    for c in cypherText:
+        asciiNum = ord(c)
+        if c in charCount:
+            charCount[c]+=1
+        else:
+            charCount[c] = 1
+        asciiNums.append(asciiNum)
+        print c,"\t\t",asciiNum
+
+
+
+    print "asciiNums: min val: ",min(asciiNums), " max val: ",max(asciiNums), " range: ",max(asciiNums) - min(asciiNums)
+    print charCount
+    print sorted(charCount.items(), key=operator.itemgetter(1), reverse=True)
+
+    # Clearly this is not a substitution cypher to few similar characters (3: 3's, 2: *'s, etc)
+
+
+
 
 
 
