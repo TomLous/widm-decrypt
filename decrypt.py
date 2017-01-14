c149a2b1e8471c87419668f1480298396d184905
Tom Lous <tomlous@gmail.com>
2017-01-14 22:08:15 +0100

    First commit

---
+++ decrypt.py
@@ -0,0 +1,15 @@
+import sys
+import binascii
+
+
+def main():
+    # Afl 2 WIDM code on wall
+
+    decodedString = ""
+
+    cypherText = "HXvfWs@4$#&Rty Pkm!nm348LgM**% GfrZ33?/?Rtc96"
+    cypherText = "HXvfW5@4$#&Rty Pkm!nm348LgM**% GfrZ33?/?Rtc96"
+
+
+
+main()
